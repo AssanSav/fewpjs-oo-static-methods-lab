71dbdf5b6a6e738e2d9ad14d4ccddee2f56183a5
AssanSav <assane.savadogo81@gmail.com>
2020-03-02 02:53:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt().toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9- ']+/g,'')
+  }
+
+  static titleize(string) {
+    let splitString = string.split(" ")
+    let result = []
+    let excludeWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    for (let i = 0; i < splitString.length; i++) {
+      if (excludeWords.includes(splitString[i])) {
+        result.push(splitString[i])
+      }
+      else {
+        result.push(splitString[i].charAt().toUpperCase() + splitString[i].slice(1))
+      }
+    }
+    return result.join(" ").charAt(0).toLocaleUpperCase() + result.join(" ").slice(1)
+  }
 }
